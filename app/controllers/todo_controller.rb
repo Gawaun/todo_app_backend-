2ab0b29bd4feb9a5b7514c97ad8b888f03e6731d
Gawaun <gino13m@icloud.com>
2017-10-21 18:08:30 +0000

    Add erb tags instance variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,10 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+    @todo_discription = "Make the cirriculm"
+    @todo_pomodoro_estimate = 3
+    end
+
+    
 end
