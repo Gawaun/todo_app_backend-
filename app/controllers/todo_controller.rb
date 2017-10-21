b99bc5921d27aba18c47e7d9accb04635423556a
Gawaun <gino13m@icloud.com>
2017-10-21 19:19:37 +0000

    Add condtional logic & elsif

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,15 @@ class TodoController < ApplicationController
     def index
     end
     def show
-    @todo_discription = "Make the cirriculm"
-    @todo_pomodoro_estimate = 3
+        todo_id = params[:id]
+        
+        if todo_id == '1'
+            @todo_discription = "Make the cirriculm"
+            @todo_pomodoro_estimate = 3
+                elsif todo_id == '2'
+            @todo_discription = "Do homework"
+            @todo_pomodoro_estimate = 4
+        end
     end
 
     
