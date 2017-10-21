827aacdb28bf5c21a75260407dc249cf626ae794
Gawaun <gino13m@icloud.com>
2017-10-21 19:55:23 +0000

    Add links to tasks pages

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,11 +7,9 @@ class TodoController < ApplicationController
         if todo_id == '1'
             @todo_discription = "Make the cirriculm"
             @todo_pomodoro_estimate = 3
-                elsif todo_id == '2'
+        elsif todo_id == '2'
             @todo_discription = "Do homework"
             @todo_pomodoro_estimate = 4
         end
     end
-
-    
 end
