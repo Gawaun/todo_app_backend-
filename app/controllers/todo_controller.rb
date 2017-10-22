b9ae7bffa2cccdb36b057c0a62de16a1d1e6279c
Gawaun <gino13m@icloud.com>
2017-10-22 15:50:27 +0000

    Add remove conditional logic & pull data from database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,14 +2,9 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_id = params[:id]
+      
+        @todo = Todo.find_by_id(params[:id])
         
-        if todo_id == '1'
-            @todo_discription = "Make the cirriculm"
-            @todo_pomodoro_estimate = 3
-        elsif todo_id == '2'
-            @todo_discription = "Do homework"
-            @todo_pomodoro_estimate = 4
-        end
+        
     end
 end
